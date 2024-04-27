88709793feb8d314f86a8d33df7d5a73bd17a055
OmarLopGem <101218351+OmarLopGem@users.noreply.github.com>
2024-04-26 20:09:32 -0600

    restrictions
    Funcion que devuelve un diccionario de las restricciones

---
+++ Stage2/restricions.py
@@ -0,0 +1,37 @@
+import random as rand
+
+def restrictionsGroups():
+    restrictions = {}
+
+    heavyRestrictions = ["scheduleCollisionProf",
+                         "scheduleCollisionStu",
+                         "breakTime",
+                         "noMoreTwoAndPairs",
+                         "availableProf",
+                         "definedSchedulesUFsAndProf",
+                         "minMaxProfTotal",
+                         "catProfModuleRel",
+                         "oddHours"]
+    
+    lightRestrictions = ["noWednesday",
+                         "plantProfModuleRel",
+                         "minCatProf",
+                         "continousSchedule",
+                         "onlyEight",
+                         "notAfterSeven"]
+    
+    for i in range(len(heavyRestrictions)):
+        randomNum = rand.randint(100, 108)
+        restrictions[heavyRestrictions[i]] = randomNum
+
+    for i in range(len(lightRestrictions)):
+        randomNum = rand.randint(10, 15)
+        restrictions[lightRestrictions[i]] = randomNum
+
+    for clave, valor in restrictions.items():
+        print(clave, ": ", valor)
+
+    return restrictions
+        
+
+restrictionsGroups()
