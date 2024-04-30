0e867d12f56162d4f3e2282bbe2f5ad9749a2613
OmarLopGem <101218351+OmarLopGem@users.noreply.github.com>
2024-04-30 16:36:26 -0600

    diccionario nombre y horas

--- Stage2/hours.py
+++ Stage2/hours.py
@@ -6,13 +6,17 @@ import re
 def hoursCourses():
     hours = {}
     mod_path = Path(__file__).parent
-    blocks = [x for x in os.listdir(f"{mod_path}/csvs") if re.match(r'^.*-Profesores.csv$', x)]
-    print (blocks)
+    #blocks = [x for x in os.listdir(f"{mod_path}/csvs") if re.match(r'^.*-Profesores.csv$', x)]
+    #print (blocks)
 
-    for _ in range(len(blocks)):
-        with open(f"{mod_path}/csvs/"+blocks[_], "r") as f:
-            lines = csv.reader(f)
-            for i, line in enumerate(lines):
+    #for _ in range(len(blocks)):
+    with open(f"{mod_path}/csvs/UDF.csv","r") as f:
+        reader = csv.DictReader(f)
+        for row in reader:
+            hours[row['Nombre']] = row['Horas']
+
+    for clave, valor in hours.items():
+        print(clave, ": ", valor)
 
 
 
