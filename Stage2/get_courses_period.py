f65ee95c48dfd5da669525d23b0d06aa8f0e4867
fabian994 <a01367585@itesm.mx>
2024-04-30 16:06:39 -0600

    modified get_courses_period output to period-courses
    get_courses_period.py output is now:
    {1:[course_list],
    2:[course_list],
    3:[course_list]}

--- Stage2/get_courses_period.py
+++ Stage2/get_courses_period.py
@@ -2,18 +2,27 @@ import pandas as pd
 from pathlib import Path
 
 def dict_courses_period(df):
-    courses_period = {}
+    p1, p2, p3 = [],[],[]
+    
     for index, row in df.iterrows():
         #print(row['Clave'],row['P1'], row['P2'], row['P3'])
         periods = [i for i,p in enumerate([row['P1'], row['P2'], row['P3']],1) if p != '-']
-        #print(periods)
-        courses_period[row['Clave']] = periods
+        for i in periods:
+            if i == 1:
+                p1.append(row['Clave'])
+            if i == 2:
+                p2.append(row['Clave'])
+            if i == 3:
+                p3.append(row['Clave'])
+    courses_period = {1:p1, 2:p2, 3:p3}
     print(courses_period)
     #return courses_period
 
 def get_courses_period(semester_even):
     mod_path = Path(__file__).parent
     df = pd.read_csv(f"{mod_path}/csvs/UDF.csv")
+    df = df.fillna('-')
+    #print(df)
     if semester_even is True:
         df = df.drop(df[(df.Semestre % 2) != 0].index)
     else:
