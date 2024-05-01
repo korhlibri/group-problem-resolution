f82c2e2e5359a179369f1ca2e11f7f5ef00f76f4
OmarLopGem <101218351+OmarLopGem@users.noreply.github.com>
2024-04-30 20:56:02 -0600

    ya basta

--- Stage2/hours.py
+++ Stage2/hours.py
@@ -2,22 +2,53 @@ import csv
 import os
 from pathlib import Path
 import re
+import numpy as np
 
-def hoursCourses():
-    hours = {}
+def hoursCoursesTopics(nombre_archivo):
+    hoursUDFTopic = {}
     mod_path = Path(__file__).parent
-    #blocks = [x for x in os.listdir(f"{mod_path}/csvs") if re.match(r'^.*-Profesores.csv$', x)]
-    #print (blocks)
+    blocks = [x for x in os.listdir(f"{mod_path}/csvs") if re.match(r'^.*-Profesores.csv$', x)]
+    with open(f"{mod_path}/csvs/{nombre_archivo}","r", encoding="utf-8") as f:
+        reader = csv.reader(f)
+        next(reader)
+        next(reader)
+        data= {}
+        
+        for row in reader:
+            tema = row[0]
+            horas = row[2]
+            data[tema] = horas
 
-    #for _ in range(len(blocks)):
+    return data   
+
+
+def hoursCoursesUDF():
+    hoursUDF = {}
+    data = {}
+    mod_path = Path(__file__).parent
+    blocks = [x for x in os.listdir(f"{mod_path}/csvs") if re.match(r'^.*-Profesores.csv$', x)]
+
+    for file in blocks:
+        clave = file.split("-")[0]
+        if "B-" in file: 
+            data = hoursCoursesTopics(file)
+        else:
+            data = {'1': 1} 
+        hoursUDF[clave] = data
+
     with open(f"{mod_path}/csvs/UDF.csv","r") as f:
         reader = csv.DictReader(f)
         for row in reader:
-            hours[row['Nombre']] = row['Horas']
+            
+            if "B" not in row["ï»¿Clave"]:
+                #dic["1"] = row['Horas']
+                hoursUDF[row['ï»¿Clave']] = {'1': row['Horas']}
 
-    for clave, valor in hours.items():
+    for clave, valor in hoursUDF.items():
         print(clave, ": ", valor)
 
+    return hoursUDF
 
 
-hoursCourses()
+#hoursCoursesTopics()
+hoursCoursesUDF()
