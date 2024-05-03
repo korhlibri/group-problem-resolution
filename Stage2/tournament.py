74d09443975cc055d7d63bece2f6af4fe58185b7
Dohko1123 <dariomejiait@outlook.com>
2024-05-03 17:11:59 -0600

    Hola

--- Stage2/tournament.py
+++ Stage2/tournament.py
@@ -10,21 +10,35 @@ def tournament(poblation, n_parents):
 
     for i in poblation:
         cromosom_sort.append(restriccions_function.calculate(i, restrictions_dict, True))
-        
+    
     cromosom_sort.sort()
     parents = []
     for _ in range(n_parents):
-        candidates = []
-        for i in range(0,5):
-            candidates.append(random.choice(cromosom_sort))
-        parent_1 = min(candidates)
+        option = random.randint(0,9)
+        if option == 9:
+            candidates = []
+            for i in range(0,5):
+                candidates.append(random.choice(cromosom_sort[6:]))
+            parent_1 = min(candidates)
 
-        candidates = []
-        for i in range(0,5):
-            candidates.append(random.choice(cromosom_sort))
-        parent_2 = min(candidates)
-        
-        parents.append((parent_1[1], parent_2[1]))
+            candidates = []
+            for i in range(0,5):
+                candidates.append(random.choice(cromosom_sort[6:]))
+            parent_2 = min(candidates)
+            
+            parents.append((parent_1[1], parent_2[1]))
+        else:
+            candidates = []
+            for i in range(0,5):
+                candidates.append(random.choice(cromosom_sort[:6]))
+            parent_1 = min(candidates)
+
+            candidates = []
+            for i in range(0,5):
+                candidates.append(random.choice(cromosom_sort[:6]))
+            parent_2 = min(candidates)
+            
+            parents.append((parent_1[1], parent_2[1]))
 
     return cromosom_sort, parents
 
