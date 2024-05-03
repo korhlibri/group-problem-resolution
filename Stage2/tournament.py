c426dfce0ac3d5975f36c037d6536f592b8932db
Dohko1123 <dariomejiait@outlook.com>
2024-05-03 15:15:03 -0600

    tournament

--- Stage2/tournament.py
+++ Stage2/tournament.py
@@ -1,51 +1,30 @@
 import create_cromosom
 import restriccions_function
 import restrictions
-import os
-import json
 import random
 
-directory = 'json'
-if not os.path.exists(directory):
-    os.makedirs(directory)
 
-def save_dict_as_json(data, filename):
-    with open(filename, 'w', encoding='utf-8') as f:
-        json.dump(data, f, indent=4)
+def tournament(poblation, n_parents):
+    restrictions_dict = restrictions.restrictionsGroups()
+    cromosom_sort = []
+
+    for i in poblation:
+        cromosom_sort.append(restriccions_function.calculate(i, restrictions_dict, True))
         
-poblation = []
-n_poblation = 100
+    cromosom_sort.sort()
+    parents = []
+    for _ in range(n_parents):
+        candidates = []
+        for i in range(0,5):
+            candidates.append(random.choice(cromosom_sort))
+        parent_1 = min(candidates)
 
-for _ in range(n_poblation):
-    poblation.append(create_cromosom.get_cromosoma(True))
+        candidates = []
+        for i in range(0,5):
+            candidates.append(random.choice(cromosom_sort))
+        parent_2 = min(candidates)
+        
+        parents.append(parent_1[1], parent_2[1])
 
-restrictions_dict = restrictions.restrictionsGroups()
+    return cromosom_sort, parents
 
-cromosom_sort = []
-
-for i in poblation:
-    cromosom_sort.append(restriccions_function.calculate(i,restrictions_dict, True))
-    
-cromosom_sort.sort()
-
-indexs = []
-for i in range(0, 50):
-    indexs.append(i)
-
-cromosom_sort = cromosom_sort[:50]
-second_generation = []
-
-for _ in range (0,25):
-    n = random.randint(0, len(indexs) - 1)
-    n = indexs.pop(n)
-    
-    
-    m = random.randint(0, len(indexs) - 1)
-    m = indexs.pop(m)
-    
-    second_generation.append((cromosom_sort[n][1], cromosom_sort[m][1]))
-    
-for i in cromosom_sort:
-    second_generation.append(i[1])
-    
-print(len(second_generation))
