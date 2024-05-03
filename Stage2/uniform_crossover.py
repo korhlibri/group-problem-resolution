99bc59e87e356f0b75610751c59d27ddc397d473
Ricardo Gonzalez <ricardo_glez_14@icloud.com>
2024-05-03 10:04:47 -0600

    Uniform Crossover modified

--- Stage2/uniform_crossover.py
+++ Stage2/uniform_crossover.py
@@ -6,18 +6,8 @@ from pprint import pprint as pp
 import json
 import json
 
-cromosomes_pots = json.load(open(".//json//cromosomas.json"))
-
-#Select 2 random cromosomes
-def select_cromosomes():
-    cromosomes = list(cromosomes_pots.keys())
-    cromosomes = random.sample(cromosomes, 2)
-    return cromosomes
-
 
 def uniform_crossover(cr1, cr2):
-    cr1 = cromosomes_pots[cr1]
-    cr2 = cromosomes_pots[cr2]
     
     # Select a random point in the cromosomes
     point = random.randint(0, len(cr1))
@@ -48,10 +38,3 @@ def uniform_crossover(cr1, cr2):
         
     return new_cromosome, new_cromosome2
     
-
-# pp(uniform_crossover(*select_cromosomes()))
-
-
-
-
-
